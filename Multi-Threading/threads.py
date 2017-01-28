65587383d83d75f4fbce3082abf1f00089372fe7
Maneesh D <maneeshd77@gmail.com>
2017-01-29 04:51:15 +0530

    threads first example

---
+++ Multi-Threading/threads.py
@@ -0,0 +1,21 @@
+"""
+@author: Maneesh D
+@email: maneeshd77@gmail.com
+"""
+
+import threading
+
+
+thread1 = threading.Thread(name='MyThread-1')
+thread1.start()
+print("Thread1 = ", thread1.getName())
+print("Thread1 is daemon = ", thread1.isDaemon())
+
+thread2 = threading.Thread(name="MyThread-2", daemon=True)
+thread1.join()
+print("Killing Thread1...")
+thread2.start()
+print("Thread2 = ", thread2.getName())
+print("Thread2 is daemon = ", thread2.isDaemon())
+thread2.join()
+print("Killing Thread2...")
