f7120d61b133e38356deb18cf91ba4b09c11a803
Maneesh D <maneeshd77@gmail.com>
2017-01-29 05:30:13 +0530

    threads first example

--- Multi-Threading/threads.py
+++ Multi-Threading/threads.py
@@ -1,23 +1,47 @@
+#!/usr/local/bin/python3.6
 """
 @author: Maneesh D
 @email: maneeshd77@gmail.com
 """
 
+
 import threading
+import time
 
 
-print("Creating Thread1...")
-thread1 = threading.Thread(name='MyThread-1')
-thread1.start()
-print("Thread1 = ", thread1.getName())
-print("Thread1 is daemon = ", thread1.isDaemon())
+class MyThread (threading.Thread):
+    def __init__(self, thread_id, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = thread_id
+        self.name = name
+        self.counter = counter
 
-print("Creating Thread2...")
-thread2 = threading.Thread(name="MyThread-2", daemon=True)
-thread1.join()
-print("Killing Thread1...")
-thread2.start()
-print("Thread2 = ", thread2.getName())
-print("Thread2 is daemon = ", thread2.isDaemon())
-thread2.join()
-print("Killing Thread2...")
+    def run(self):
+        print("Starting " + self.name)
+        print_time(self.name, self.counter, 5)
+        print("Exiting " + self.name)
+
+
+def print_time(thread_name, delay, counter):
+    while counter:
+        time.sleep(delay)
+        print("%s: %s" % (thread_name, time.ctime(time.time())))
+        counter -= 1
+
+
+def main():
+    print(threading.current_thread().getName())
+
+    # Create new threads
+    thread1 = MyThread(1, "Thread-1", 1)
+    thread2 = MyThread(2, "Thread-2", 2)
+
+#    Start new Threads
+    thread1.start()
+    thread2.start()
+    thread1.join()
+    thread2.join()
+    print("Exiting Main Thread")
+
+if __name__ == "__main__":
+    main()
