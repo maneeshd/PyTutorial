8ceb81fd373e5c60cee8273d781307f500e2be5a
maneeshd <maneeshd77@gmail.com>
2017-02-14 17:37:05 +0530

    Threading and Multiprocessing

--- Advanced/Multi-Threading & Processing/multiprocessing/multiprocessing_Process.py
+++ Advanced/Multi-Threading & Processing/multiprocessing/multiprocessing_Process.py
@@ -3,11 +3,12 @@
 @email: maneeshd77@gmail.com
 @created: 10-Feb-17
 """
-from multiprocessing import Process
+from multiprocessing import Process, current_process
 from time import time, ctime, sleep
 
 
-def timer(name, delay, repeat):
+def timer(delay, repeat):
+    name = current_process().name
     print("[INFO] " + name + " started")
     while repeat > 0:
         sleep(delay)
@@ -17,13 +18,11 @@ def timer(name, delay, repeat):
 
 
 def main():
-    process1 = Process(target=timer, args=("Process-1", 1, 5))
-    process2 = Process(target=timer, args=("Process-2", 2, 5))
+    process1 = Process(target=timer, args=(1, 5,), name="Process_01")
+    process2 = Process(target=timer, args=(2, 5,), name="Process_02")
     process1.start()
     process2.start()
-    print("[INFO] MainProcess Started")
-    timer("MainTimer", 1, 5)
-    print("[INFO] MainProcess Exited")
+    timer(1, 5)
 
 if __name__ == '__main__':
     main()
