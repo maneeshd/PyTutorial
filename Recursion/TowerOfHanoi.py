90b7f1cf2f34ea6d75ce1cbf3fda53f4b826919e
maneeshd <maneeshd77@gmail.com>
2018-01-18 18:54:03 +0530

    Variable name changes in Tower Of Hanoi

--- Recursion/TowerOfHanoi.py
+++ Recursion/TowerOfHanoi.py
@@ -6,7 +6,7 @@
 
 def tower_of_hanoi(disks, source, dest, aux):
     if disks == 1:
-        print("Move disk 1 from " + source + " rod to " + dest + " rod.")
+        print("Move disk 1 from " + source + " to " + dest + ".")
         return
     tower_of_hanoi(disks - 1, source, aux, dest)
     print(
@@ -14,12 +14,12 @@ def tower_of_hanoi(disks, source, dest, aux):
         str(disks) +
         " from " +
         source +
-        " rod to " +
+        " to " +
         dest +
-        " rod.")
+        ".")
     tower_of_hanoi(disks - 1, aux, dest, source)
 
 
 if __name__ == '__main__':
     num_of_disks = int(input("Enter the number of disks: "))
-    tower_of_hanoi(num_of_disks, "A", "C", "B")
+    tower_of_hanoi(num_of_disks, "src", "aux", "dest")
